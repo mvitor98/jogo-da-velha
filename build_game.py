a3e2e541b45ab6d8db844f7f95d7cbd17023312a
mvitor98 <marciosantosvitoralves@gmail.com>
2022-08-01 01:04:41 -0300

    adicionado formatação na apresentação da matriz

--- build_game.py
+++ build_game.py
@@ -12,11 +12,12 @@ class Matrix:
         return list(self.__build_matrix_line() for i in range(self._spots))
     
     def _show_matrix(self) -> None:
-        for row in self._matrix:
-            print(row)
+        print('   1  2  3')
+        for index, row in enumerate(self._matrix, 1):
+            print(index, row)
 
 if __name__ == "__main__":
     jogo = Matrix()
     jogo._show_matrix()
-    print(len(jogo._matrix))
+    # print(len(jogo._matrix))
     
